6da8897b14995d2600f36e4eba784c99676e8b68
krishnarajmitosis <krishna.v@neuralrays.ai>
2021-07-15 23:10:26 +0530

    build file updated

--- android/app/src/main/kotlin/com/neuralraysai/ms_centerapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/neuralraysai/ms_centerapp/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.neuralraysai.ms_centerapp
 
+import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // 2
+        super.onCreate(savedInstanceState)
+        AppCenter.start(getApplication(), "a8c09c1d-816e-4e09-86b1-24057adb0ef6",
+                Analytics::class.java, Crashes::class.java)
+    }
 }
